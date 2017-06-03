b8d2eeb1118aa8b4d561ceaae8a508339bce598b
Alexander R. Torrijos <artorrijos@gmail.com>
2017-06-03 12:07:06 +0000

    Add album working

--- config/modules.config.php
+++ config/modules.config.php
@@ -11,6 +11,10 @@
  * This should be an array of module namespaces used in the application.
  */
 return [
+    'Zend\I18n',
+    'Zend\Form',
+    'Zend\Hydrator',
+    'Zend\Filter',
     'Zend\Db',
     'Zend\Router',
     'Zend\Validator',
